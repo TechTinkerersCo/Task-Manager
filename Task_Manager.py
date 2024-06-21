a6575b306cc018da604e61d4ed0d7e280f10f752
Ritvikisalive-coding <ritviksingh090@gmail.com>
2024-06-21 13:52:43 +1000

    Creation of python file

---
+++ Task_Manager.py
@@ -0,0 +1,12 @@
+from customtkinter import *
+from tkinter import *
+
+root = CTk()
+root.title("Task Manager")
+root.iconbitmap('images/logo.ico')
+root.geometry("400x400")
+
+
+
+
+root.mainloop()
